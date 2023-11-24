d8065f22dc4b81199deffb1d3ffbdcbce6c94293
SpeedWagon12 <151126812+SpeedWagon12@users.noreply.github.com>
2023-11-24 15:56:33 +0300

    Add files via upload

---
+++ Zadanie 1/Zadanie 1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(NumberA.Text, out int l))
+            {
+                MessageBox.Show("Введите число!");
+            }
+            int m;
+
+            m = l / 100;
+
+            Answer.Text += $"{m} [Метров]\n";
+
+        }
+    }
+}
---
+++ Zadanie 2/Zadanie 2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(NumberA.Text, out int x))
+            {
+                MessageBox.Show("Введите число!");
+            }
+            int m, b, z;
+            m = x % 10;
+            b = x / 10 % 10;
+            z = x / 100;
+            Answer.Text += $"{m}{b}{z}";
+        }
+    }
+}
